76bd5df5da82de8b48a79f1bb836bcae1a688323
pksung1 <pksung1@gmail.com>
2021-08-31 02:19:17 +0900

    build

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,43 @@
 import Layout from '../../components/layout'
+import { getPostdata, getAllPostIds } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
-export default function Post() {
-  return <Layout>...</Layout>
+
+export default function Post({postData}) {
+  return (<Layout>
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    
+    <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+  </Layout>)
 }
 
 export async function getStaticPaths() {
     // Return a list of possible value for id
-  }
+    // Possible values for id 가능안 아이디 목록을 보냅니다. 다시말해 가능한 paths들을 보냅니다.
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false
+    }
+}
   
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = await getPostdata(params.id)
+
+  return {
+    props: {
+      postData
+    }
   }
+}
